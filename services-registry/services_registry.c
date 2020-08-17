ef7d1ed647756a1529ca6b4da8e4a90f40a3223a
balemarthy <krishna.bv@gmail.com>
2020-08-18 03:02:29 +0530

    Folder and files created for services registry project

---
+++ services-registry/services_registry.c
@@ -0,0 +1,6 @@
+#include "services_registry.h"
+
+
+int main(void) {
+    return 0;
+}
---
+++ services-registry/services_registry.h
@@ -0,0 +1,9 @@
+#ifndef __SERVICES_REGISTRY_H_
+#define __SERVICES_REGISTRY_H_
+
+#include <stdio.h>
+#include <stdint.h>
+
+
+
+#endif
