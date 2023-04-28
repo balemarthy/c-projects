c83d2a6dd23ea62819d275b3dd08089073aff225
B Vamsi Krishna <krishna.bv@gmail.com>
2023-04-29 02:01:43 +0530

    Update proto.c
    Added helper functions and APIs that will be called by code using this protocol

--- get-set-protocol/proto.c
+++ get-set-protocol/proto.c
@@ -15,9 +15,46 @@ uint8_t get_length(void);
 uint8_t *get_value(void);
 uint16_t get_chksum(void);
 
+/* Helper functions */
+uint8_t create_msg();
+uint8_t calculate_chksum();
+
+/* APIs */
+uint8_t frame_packet();
+uint8_t parse_packet();
+
 // Temporarily silence the linker. The protocol files are supposed to get integrated with app file
 int main(void) {
 
     return 0;
 
+}
+
+uint8_t create_msg() {
+    if(NULL == msg) {
+        msg = (msg_fmt *)malloc(sizeof(msg_fmt));   
+        return (1);
+    } else {
+        // msg already created - singleton pattern
+        return (0);
+    }
+}
+
+uint8_t frame_packet(struct msg_fields *msg_fields) {
+    if(NULL == msg_fields) {
+        return -1;
+    } else {
+        if(create_msg()) {
+            set_opcode(msg_fields->opcode);
+            set_type(msg_fields->type);
+            set_length(msg_fields->length);
+            set_value(msg_fields->value);
+            
+            if(calculate_chksum()) {
+                set_chksum();
+            } else {
+                set_chksum(DEF_CHKSUM);
+            }
+        }   
+    }
 }
