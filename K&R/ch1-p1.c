8f432b7655988f67a1a9d4ba6ba4e7ff69597125
B Vamsi Krishna <krishna.bv@gmail.com>
2023-05-06 22:47:27 +0530

    Create ch1-p1.c
    This is the first program in K&R C language book. This is a very primitive program. 
    This folder contains all the code from K&R C, both the exercises and code from lessons.

---
+++ K&R/ch1-p1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>                /* include information about standard library */
+
+int main(void)                    /* define a function called main that received no argument values */
+{                                 /* statements of main are enclosed in braces */
+    printf("hello, world\n");     /* main calls library function printf to print this sequence of characters */
+}                                 /* \n represents the newline character */
