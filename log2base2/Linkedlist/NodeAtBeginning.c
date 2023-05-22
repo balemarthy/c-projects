754e404cd2e43bcc7fc38c0af0f899c3c9d5328d
B Vamsi Krishna <krishna.bv@gmail.com>
2023-05-22 23:14:35 +0530

    Create NodeAtBeginning.c
    This program creates Linked List by adding nodes at the beginning.

---
+++ log2base2/Linkedlist/NodeAtBeginning.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    struct node
+    {
+        int data;
+        struct node *next;
+    };
+
+    //declaring nodes
+    struct node *a,*b,*c;
+    a = (struct node *)malloc(sizeof(struct node));
+    b = (struct node *)malloc(sizeof(struct node));
+    c = (struct node *)malloc(sizeof(struct node));
+
+    //Implement task 1 here
+    a->data = 100;
+    b->data = 200;
+    c->data = 300;
+
+    //Implement task 2 here
+    c->next = a;
+    a->next = b;
+    b->next = NULL;
+
+    //Don't change the below code
+    struct node *temp = c;
+
+    while(temp != NULL)
+    {
+        printf("%d ",temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
