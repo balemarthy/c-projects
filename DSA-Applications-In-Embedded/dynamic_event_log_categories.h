345fba135bfadb634b584b66ae1ab6d59d9d6a23
B Vamsi Krishna <krishna.bv@gmail.com>
2025-01-27 22:35:00 +0530

    Create dynamic_event_log_categories.h
    Header file (optional) to categorize various kinds of events

---
+++ DSA-Applications-In-Embedded/dynamic_event_log_categories.h
@@ -0,0 +1,14 @@
+/**
+ * @file categories.h
+ * @brief Defines event category bitmasks for logging.
+ */
+
+#ifndef CATEGORIES_H
+#define CATEGORIES_H
+
+#define EVENT_ERROR   (1 << 0)
+#define EVENT_SENSOR  (1 << 1)
+#define EVENT_INFO    (1 << 2)
+// Add more categories as needed
+
+#endif /* CATEGORIES_H */
