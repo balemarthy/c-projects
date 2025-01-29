737073b948fc4dedfdada3a998edaab2ea241965
B Vamsi Krishna <krishna.bv@gmail.com>
2025-01-29 21:53:34 +0530

    Create dynamic_event_log_cmd_parser.h
    Command parser header file

---
+++ DSA-Applications-In-Embedded/dynamic_event_log_cmd_parser.h
@@ -0,0 +1,14 @@
+/**
+ * @file command_parser.h
+ * @brief Declarations for handling incoming UART commands.
+ */
+
+#ifndef COMMAND_PARSER_H
+#define COMMAND_PARSER_H
+
+/**
+ * @brief Checks UART input for command strings and acts accordingly.
+ */
+void checkAndHandleCommands(void);
+
+#endif /* COMMAND_PARSER_H */
